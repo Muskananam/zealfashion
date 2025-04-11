3851490e4d980a62654e5d23afb2542a4c34deff
Muskan Anam <mb84338433@gmail.com>
2025-04-11 09:28:15 +0530

    Signup.js

--- backend/Models/Signup.js
+++ backend/Models/Signup.js
@@ -4,16 +4,19 @@ const signupSchema = new mongoose.Schema({
   name: {
     type: String,
     required: true,
+    trim: true
   },
   email: {
     type: String,
     required: true,
     unique: true,
+    lowercase: true, //  ensures consistent storage
+    trim: true
   },
   password: {
     type: String,
-    required: true,
-  },
+    required: true
+  }
 });
 
 module.exports = mongoose.model("Signup", signupSchema);
