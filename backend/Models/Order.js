c42e28ad866089e768d90e407521cc7eff539d2c
Muskan Anam <mb84338433@gmail.com>
2025-04-11 09:30:40 +0530

    Order.js

--- backend/Models/Order.js
+++ backend/Models/Order.js
@@ -1,1 +1,25 @@
+const mongoose = require('mongoose');
+
+const orderSchema = new mongoose.Schema({
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  product: {
+    productId: String,
+    name: String,
+    price: Number,
+    quantity: Number,
+  },
+  address: {
+    fullName: String,
+    street: String,
+    city: String,
+    state: String,
+    zip: String,
+    country: String,
+  },
+  paymentMethod: String,
+  status: { type: String, default: 'Pending' },
+  createdAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('Order', orderSchema);
 
