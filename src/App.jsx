7cd34e1d138837364755084e131bd3d97294d20f
Muskan Anam <mb84338433@gmail.com>
2025-04-11 09:35:58 +0530

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,24 +8,27 @@ import Signin from "./pages/Signin";
 import Signup from "./pages/Signup";
 import NotFound from "./pages/NotFound";
 import { CartProvider } from "./context/CartContext";
-import "./App.css";
-
 import Aboutus from './pages/Aboutus';
+import BuyNowPage from "./pages/BuyNow";
+import "./App.css";
 
 function App() {
   return (
     <CartProvider>
       <Router>
         <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/products" element={<Products />} />
-          <Route path="/cart" element={<Cart />} />
-          <Route path="/signin" element={<Signin />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/about" element={<Aboutus />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
+        <div className="pt-24">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/products" element={<Products />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/signin" element={<Signin />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/about" element={<Aboutus />} />
+            <Route path="/buy-now" element={<BuyNowPage />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </div>
       </Router>
     </CartProvider>
   );
